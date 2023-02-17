798bca56933ef56bf74e3e6da4d28c44fd265d27
Ron Shabi <ron@ronsh.net>
2023-02-17 05:00:47 +0200

    Barebones version

---
+++ history.py
@@ -0,0 +1,26 @@
+DEFAULT_FILE_LOCATION = "/var/log/zypp/history"
+
+DISCARDED_CHARS = ["#", "zypper", "yast"]
+try:
+    with open(DEFAULT_FILE_LOCATION) as f:
+        lines = f.readlines()
+        
+        # eat lines with discarded chars
+        lines = [line for line in lines if not any(c in line for c in DISCARDED_CHARS)]
+
+        # eat lines with discarded words
+        lines = [line for line in lines if not any(word.lower() in DISCARDED_CHARS for word in line)]
+
+        for line in lines:
+            elems = line.split('|')
+            
+            entry_date = elems[0]
+            entry_action = elems[1]
+            entry_package = elems[2]
+            entry_version = elems[3]
+            # entry_arch = elems[4]
+
+            print(f"{entry_date}\t{entry_action}\t\t{entry_package} ({entry_version})")
+
+except PermissionError:
+    print(f"Insufficient privileges to access zypper history file ({DEFAULT_FILE_LOCATION}). Are you root?")
