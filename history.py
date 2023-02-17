bbecae2be409c16af1308adda0656d50f101c489
Ron Shabi <ron@ronsh.net>
2023-02-17 05:06:06 +0200

    Formatting

--- history.py
+++ history.py
@@ -1,15 +1,15 @@
 DEFAULT_FILE_LOCATION = "/var/log/zypp/history"
+DISCARDED = ["#", "zypper", "yast"]
 
-DISCARDED_CHARS = ["#", "zypper", "yast"]
 try:
     with open(DEFAULT_FILE_LOCATION) as f:
         lines = f.readlines()
         
         # eat lines with discarded chars
-        lines = [line for line in lines if not any(c in line for c in DISCARDED_CHARS)]
+        lines = [line for line in lines if not any(c in line for c in DISCARDED)]
 
         # eat lines with discarded words
-        lines = [line for line in lines if not any(word.lower() in DISCARDED_CHARS for word in line)]
+        lines = [line for line in lines if not any(word.lower() in DISCARDED for word in line)]
 
         for line in lines:
             elems = line.split('|')
